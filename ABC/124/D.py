a192aa7eefe4194e108ee0692457ba13adf55ce1
Kenj-I <kenji.iwase.5@gmail.com>
2019-04-15 13:07:49 +0900

    [update] add answer abc124 D

---
+++ ABC/124/D.py
@@ -0,0 +1,33 @@
+# reffer https://qiita.com/hironobukawaguchi3/items/55e751ecf72bb7894a46
+N, K = map(int, input().split())
+S = input()
+Nums = []
+now = 1 # 今見ている数
+cnt = 0 # nowがいくつならんでいるか
+
+for i in range(N):
+  if S[i] == str(now):
+    cnt+=1
+  else:
+    Nums.append(cnt)
+    now = 1 - now
+    cnt = 1
+
+if cnt != 0:
+  Nums.append(cnt)
+
+if len(Nums) % 2 == 0:
+  Nums.append(0)
+
+Add = 2 * K + 1
+sum = [0] * (len(Nums) + 1)
+for i in range(len(Nums)):
+  sum[i + 1] = sum[i] + Nums[i]
+ans = 0
+for i in range(0, len(Nums), 2):
+  left = i
+  right = min(i + Add, len(Nums))
+  tmp = sum[right] - sum[left]
+  ans = max(tmp, ans)
+
+print(ans)
